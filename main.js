360ec1ec8b6544c6923fdbf8cca8d93ad1a34073
mitchellryals <mitchellryals@gmail.com>
2019-01-29 10:40:36 -0500

    Added debug mode, currently working on for loop to look for the information we need to find

--- main.js
+++ main.js
@@ -1,32 +1,60 @@
 const https = require('https');
-const url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States"
-//const url = "/home/mitchell/Documents/Python/CapstoneProject/wiki.html"
+const DEBUG_MODE = true;
 
-https.get(url, (scrape) => {
-  var data = '';
+if (DEBUG_MODE)
+  localDebug();
+else {
+  getWebpage();
+}
 
-  scrape.on('data', (piece) => {
-    data += piece;
+function localDebug() {
+  //if debug mode is enabled this function will run and retrieve a local file
+  var fs = require('fs');
+
+  try {
+      var data = fs.readFileSync('wiki.html', 'utf8');
+      var data = cleanData(data);
+  } catch(e) {
+      console.log('Error:', e.stack);
+  }
+}
+
+function getWebpage() {
+  //if debug mode is off this will retrieve whatever is in the url constant
+  const url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States"
+
+  https.get(url, (scrape) => {
+    var data = '';
+
+    scrape.on('data', (piece) => {
+      data += piece;
+    });
+
+    scrape.on('end', () => {
+      data = cleanData(data);
+      console.log(data);
+    });
+
+  }).on("error", (e) => {
+    console.log("Error: " + e.message);
   });
+}
 
-  scrape.on('end', () => {
-    data = cleanData(data);
-    console.log(data);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
 
 function cleanData(data) {
   //var newData = data.substring(0, data.indexOf("<footer"));
-  var numbers = new RegExp('[0-9]');
-  var newData = data.split("<td>");
+  const tdCheck = new RegExp('\<tr');
+  const numbers = new RegExp(/\d/g);
+  var newData = data.split(tdCheck);
+  var cleanedData = '';
 
   for (var i = 0; i < newData.length; i++) {
     //newData[i] = newData[i].substring(0, data.indexOf(RegExp))
-    newData[i] = newData[i].exec(RegExp);
-
+    //clanedData = newData[i] = newData[i].exec(RegExp) + '\n';
+    if (numbers.test(newData[i])) {
+      var output = newData[i].match(numbers);
+      console.log('found: ' + output);
+    }
   }
 
   return cleanedData;
